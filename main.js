ed4ff278aaf5f6256f78799e883ebe1fd9ea52e2
Tasneem205 <tasneemabdeltawab205@gmail.com>
2026-08-22 07:39:53 +0300

    cloudflare canceled

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import errorHandler from "./src/middleWares/errorHandler.js";
 import morgan from "morgan";
 import cors from "cors";
-import { httpServerHandler } from 'cloudflare:node';
 
 
 const app = Express();
@@ -21,12 +20,4 @@ app.use("/weather", router);
 
 app.use(errorHandler);
 
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
-// Export the fetch handler for Cloudflare Workers in production
-export default {
-  fetch: httpServerHandler(app)
-};
+app.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`));
